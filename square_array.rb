cdbfa503db50b63be2c5bc59b784e66860c5d391
William J Teskey <willjteskey@gmail.com>
2018-04-13 21:37:43 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,15 @@
 def square_array(array)
-  # your code here
+  numbers = [1, 2, 3]
+numbers_2 = [9,10,16,25]
+
+def square_array(array)
+  square_numbers = []  # Create new "empty" array
+  array.each do |num|
+    square_numbers << (num ** 2)  # Use shovel(<<) or #push method to add element to end of new array. The #each method
+    # will loop three(3) times (number of elements in array) and keep adding the new "squared" number to the end, i.e. [1],
+    # [1,4] and [1,4,9].
+  end
+  square_numbers  # Return new "square" array
 end
+
+
